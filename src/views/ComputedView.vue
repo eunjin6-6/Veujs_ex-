3e7cca69dfff8c655b891357f125135c38c92ad8
eunjin6-6 <dmswls616@gmail.com>
2024-11-15 12:23:23 +0900

    computed 파일추가

---
+++ src/views/ComputedView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { reactive, computed } from 'vue';
+
+const author = reactive ({
+  name: '홍길동',
+  books: [
+    'Vue 1 - Vue Basic',
+    'Vue 2 - Vue Guide',
+    'Vue 3 - Vue Advanced',
+  ]
+})
+
+const myfunc = computed(() => {
+  return author.books.length > 0 ? '있습니다.' : '없습니다.'
+})
+</script>
+
+<template>
+  <div class="about">
+    <h1>계산된 속성</h1>
+    <p>책이 있습니까?</p>
+    <span>{{ myfunc }}</span>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
